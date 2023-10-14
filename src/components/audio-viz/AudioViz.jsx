042944a32235ed75db8e486bfaa44c213d9c35e7
fibre-ether <fire.breather@rediff.com>
2023-10-14 11:40:06 +0530

    add config to leva and fix bounds

--- src/components/audio-viz/AudioViz.jsx
+++ src/components/audio-viz/AudioViz.jsx
@@ -1,8 +1,8 @@
 import { Bounds } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { useContext, useEffect, useMemo, useRef } from "react";
-import { ShaderMaterial } from "three";
-import { useControls } from "leva";
+import { useContext, useMemo, useRef } from "react";
+import { IcosahedronGeometry, ShaderMaterial } from "three";
+import { button, folder, useControls } from "leva";
 import { DemoFoldersContext } from "../../App";
 import { max } from "lodash-es";
 import vertexShader from "./shaders/audio-viz/vertex.glsl";
@@ -11,19 +11,25 @@ import gsap from "gsap";
 
 function AudioViz() {
   const ref = useRef();
-  const demoFolders = useContext(DemoFoldersContext);
-
-  const sphereResolution = 64;
-
   const AudioContext = useRef(null);
   const audioContent = useRef(null);
+
+  const demoFolders = useContext(DemoFoldersContext);
+
   var permission = false;
   var frequencyArray;
   var analyser;
+  var audioStream;
+
+  let currentMax;
+  let sphereResolution = 64;
+
+  let micMin = 50;
+  let micMax = 200;
 
   var soundAllowed = (stream) => {
     permission = true;
-    var audioStream = audioContent.current.createMediaStreamSource(stream);
+    audioStream = audioContent.current.createMediaStreamSource(stream);
     analyser = audioContent.current.createAnalyser();
     var fftSize = 32;
 
@@ -38,75 +44,78 @@ function AudioViz() {
     console.log(error);
   };
 
-  useEffect(() => {
-    if (!permission) {
-      navigator.mediaDevices
-        .getUserMedia({ audio: true })
-        .then(soundAllowed)
-        .catch(soundNotAllowed);
+  const updateGeometry = (ref, size) => {
+    const icosahedron = new IcosahedronGeometry(1, size, size);
+    ref.current.geometry = icosahedron;
+  };
 
-      AudioContext.current = window.AudioContext || window.webkitAudioContext;
-      audioContent.current = new AudioContext.current();
-    }
-  }, []);
-
-  // const [{ waveSpeed, randomness, waveDensity, sphereResolution }, set] =
-  useControls(() => ({
+  //eslint-disable-next-line
+  const [_, set] = useControls(() => ({
     ...demoFolders,
-    // config: folder({
-    //   waveSpeed: {
-    //     value: 0.5,
-    //     min: 0,
-    //     max: 5.0,
-    //     step: 0.1,
-    //   },
-    //   randomness: {
-    //     value: 2.75,
-    //     min: 0.0,
-    //     max: 10.0,
-    //     step: 0.1,
-    //   },
-    //   waveDensity: {
-    //     value: 2.0,
-    //     min: 0.0,
-    //     max: 10.0,
-    //     step: 0.1,
-    //   },
-    //   sphereResolution: {
-    //     value: 64,
-    //     min: 8,
-    //     max: 128,
-    //     step: 10,
-    //   },
-    //   reset: button(() =>
-    //     set({
-    //       waveSpeed: 0.5,
-    //       randomness: 2.75,
-    //       waveDensity: 2.0,
-    //       sphereResolution: 64,
-    //     })
-    //   ),
-    // }),
+    config: folder({
+      micMin: {
+        value: 50,
+        min: 0,
+        max: 100,
+        label: "Min limit",
+        onChange: (e) => {
+          micMin = e;
+        },
+      },
+      micMax: {
+        value: 200,
+        min: 101,
+        max: 300,
+        label: "Max limit",
+        onChange: (e) => {
+          micMax = e;
+        },
+      },
+      sphereResolution: {
+        value: 64,
+        min: 0,
+        max: 128,
+        label: "Resolution",
+        onChange: (e) => {
+          updateGeometry(ref, e);
+        },
+      },
+      ["Allow/Revoke Mic"]: button(() => {
+        if (!permission) {
+          navigator.mediaDevices
+            .getUserMedia({ audio: true })
+            .then(soundAllowed)
+            .catch(soundNotAllowed);
+          AudioContext.current =
+            window.AudioContext || window.webkitAudioContext;
+          audioContent.current = new AudioContext.current();
+        } else {
+          audioStream.context.close();
+          permission = false;
+        }
+      }),
+      "reset settings": button(() => {
+        micMin = 50;
+        micMax = 200;
+        updateGeometry(ref, 64);
+        set({ micMin: 50, micMax: 200, sphereResolution: 64 });
+      }),
+    }),
   }));
 
   const material = useMemo(() => {
     return new ShaderMaterial({
       uniforms: {
         uTime: { value: 0 },
-        // uWaveSpeed: { value: waveSpeed },
-        // uRandomness: { value: randomness },
-        // uWaveDensity: { value: waveDensity },
         uSoundIntensity: { value: 0 },
       },
       vertexShader,
       fragmentShader,
-      // wireframe: true,
     });
   }, []);
 
-  const micMin = 50;
-  const micMax = 200;
-  let currentMax;
+  const soundIntensityUniform = material.uniforms.uSoundIntensity;
+
   const remap = (value) => {
     const remappedValue = (value - micMin) / (micMax - micMin);
     const result = Math.min(Math.max(remappedValue, 0), 1);
@@ -116,13 +125,14 @@ function AudioViz() {
   useFrame(({ clock }) => {
     ref.current.rotation.z = clock.elapsedTime * 0.5;
     ref.current.rotation.x = clock.elapsedTime * 0.35;
+    if (permission) {
+      material.uniforms.uTime.value = clock.elapsedTime;
 
-    analyser?.getByteFrequencyData(frequencyArray);
-    currentMax = remap(max(frequencyArray));
-
-    material.uniforms.uTime.value = clock.elapsedTime;
-    const soundIntensityUniform = material.uniforms.uSoundIntensity;
-
+      analyser?.getByteFrequencyData(frequencyArray);
+      currentMax = remap(max(frequencyArray) || 0);
+    } else {
+      currentMax = 0;
+    }
     gsap.to(soundIntensityUniform, {
       duration: 0.9,
       ease: "Slow.easeOut",
@@ -131,7 +141,7 @@ function AudioViz() {
   });
   return (
     <>
-      <Bounds fit clip observe margin={1.8}>
+      <Bounds fit clip observe margin={1.1}>
         {/* eslint-disable-next-line */}
         <mesh ref={ref} material={material}>
           <icosahedronGeometry
